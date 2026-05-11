75470cfb51d4ae08d91c389e5eb97f6753034e4c
micek2021 <mikolajmiedziak@gmail.com>
2026-05-11 12:13:16 +0200

    chore: rename groupId to com.coffeeshop

--- eureka-server/src/main/java/com/coffeeshop/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/coffeeshop/eureka/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package pwr.wit.sar.eureka;
+package com.coffeeshop.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
